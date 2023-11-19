f565f689613286047316a9b59776e802545557c3
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2023-11-19 18:56:33 +0200

    Add getArrayOfNeighbors logic

--- app/logic.ts
+++ app/logic.ts
@@ -1,8 +1,12 @@
 import { CellPosition } from './types'
 
+// TODO update all neighbor Arrays to Sets
+
 export const generateNextGrid = (prevGrid: number[][]): number[][] => {
     // Make a copy of the prev grid, to apply rules simultaneously
     const nextGrid = prevGrid.map((arr) => [...arr])
+    // Store all neighbors searched during the alive cell check, for optimized revive search
+    let discoveredNeighbors: CellPosition[] = []
 
     // Check if alive cells will survive or die
     for (let row = 0; row < prevGrid.length; row++) {
@@ -23,12 +27,34 @@ export const generateNextGrid = (prevGrid: number[][]): number[][] => {
     return nextGrid
 }
 
-const getArrayOfNeighborIndices = (cellRow: number, cellColumn: number): CellPosition[] => {
+// Returns an array of CellPositions representing the neighbors of a given cell.
+// Excludes out-of-bounds neighbors and the cell itself.
+const getArrayOfNeighbors = (
+    cellRow: number,
+    cellColumn: number,
+    GRID_HEIGHT: number,
+    GRID_WIDTH: number
+): CellPosition[] => {
+    let neighborPositions: CellPosition[] = []
     for (let rowOffset = -1; rowOffset < 2; rowOffset++) {
         for (let colOffset = -1; colOffset < 2; colOffset++) {
-            //TODO
+            // Skip the cell itself
+            if (rowOffset === 0 && colOffset === 0) {
+                continue
+            }
+            // Skip out-of-bounds neighbors
+            if (
+                cellRow + rowOffset < 0 ||
+                cellRow + rowOffset >= GRID_HEIGHT ||
+                cellColumn + colOffset < 0 ||
+                cellColumn + colOffset >= GRID_WIDTH
+            ) {
+                continue
+            }
+            // Add each neighbor
+            neighborPositions.push({ row: cellRow + rowOffset, column: cellColumn + colOffset })
         }
     }
-    // TODO
-    return [{ row: 0, column: 0 }]
+    console.log(neighborPositions)
+    return neighborPositions
 }
