7d8f33189e89ebd28d3a5cf31a9621e3491e4855
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2023-11-20 17:30:10 +0200

    Fix styling of header and footer

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from 'next/image'
 import React, { useEffect, useRef } from 'react'
+import { FaGithub, FaTwitter } from 'react-icons/fa' // Importing icons from React Icons
 
 import GameOfLife from './GameOfLife'
 
@@ -51,14 +52,30 @@ const Home = () => {
 
     return (
         <div ref={backgroundRef} className="starry-background flex flex-col min-h-screen">
-            <header className="bg-gray-800 text-white text-center py-4">
-                <h1 className="text-xl font-bold">Conway's Game of Life</h1>
+            <header className="bg-offBlack-dark text-center py-4">
+                <h1 className="text-xl text-lumoGreen font-bold">Conway's Game of Life</h1>
             </header>
             <main className="flex min-h-screen flex-col items-center justify-between p-24">
                 <GameOfLife width={10} height={10} />
             </main>
-            <footer className="bg-gray-200 text-center py-4 text-sm text-gray-600 sticky bottom-0">
-                Built with ❤️ by Ben Sparks
+            <footer className="bg-offBlack-dark text-center py-4 text-sm sticky bottom-0">
+                <p className="text-lumoGreen">Built with ❤️ by Ben Sparks</p>
+                <div className="flex justify-center mt-2">
+                    <a
+                        href="https://github.com/BenSparksCode"
+                        className="mx-2 text-lumoGreen"
+                        aria-label="GitHub"
+                    >
+                        <FaGithub size={20} />
+                    </a>
+                    <a
+                        href="https://twitter.com/bensparks_"
+                        className="mx-2 text-lumoGreen"
+                        aria-label="Twitter"
+                    >
+                        <FaTwitter size={20} />
+                    </a>
+                </div>
             </footer>
         </div>
     )
