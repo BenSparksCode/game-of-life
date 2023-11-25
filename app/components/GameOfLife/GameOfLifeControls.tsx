14c06933355185c7d081f563793937fb195e63f0
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2023-11-25 22:53:25 +0200

    Add shadcn/ui and slider to controls

--- app/components/GameOfLife/GameOfLifeControls.tsx
+++ app/components/GameOfLife/GameOfLifeControls.tsx
@@ -1,16 +1,24 @@
 'use client'
 
+import { Slider } from '@/components/ui/slider'
 import { GameOfLifeControlsProps } from '../../types/types'
 
-const GameOfLifeControls: React.FC<GameOfLifeControlsProps> = ({ onNextGen }) => (
-    <div className="flex w-full place-content-center">
-        <button
-            className="mt-4 border-2 border-lumoGreen text-lumoGreen hover:bg-lumoGreen hover:text-offBlack-dark font-bold py-2 px-4 rounded"
-            onClick={onNextGen}
-        >
-            Next Gen
-        </button>
-    </div>
-)
+const GameOfLifeControls: React.FC<GameOfLifeControlsProps> = ({ onNextGen }) => {
+    const logChange = (newValue: number[]) => {
+        console.log(newValue)
+    }
+
+    return (
+        <div className="flex w-full place-content-center">
+            <Slider defaultValue={[10]} onValueChange={logChange} />
+            <button
+                className="mt-4 border-2 border-lumoGreen text-lumoGreen hover:bg-lumoGreen hover:text-offBlack-dark font-bold py-2 px-4 rounded"
+                onClick={onNextGen}
+            >
+                {'>>'}
+            </button>
+        </div>
+    )
+}
 
 export default GameOfLifeControls
