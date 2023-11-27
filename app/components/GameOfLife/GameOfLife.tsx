4d47acb62368795cdf81b9e6791d3b7cbe53a007
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2023-11-27 14:48:48 +0200

    Clean up comments and formatting

--- app/components/GameOfLife/GameOfLife.tsx
+++ app/components/GameOfLife/GameOfLife.tsx
@@ -7,15 +7,13 @@ import GameOfLifeGrid from './GameOfLifeGrid'
 import GameOfLifeControls from './GameOfLifeControls'
 
 const GameOfLife: React.FC<GameOfLifeProps> = ({ width, height }) => {
-    
     const [grid, setGrid] = useState<boolean[][]>(
         new Array(width).fill(false).map(() => new Array(height).fill(false))
     )
     const gridRef = useRef<boolean[][]>(grid)
-    const [gridHistory, setGridHistory] = useState<boolean[][][]>(
-        new Array<boolean[][]>()
-    )
+    const [gridHistory, setGridHistory] = useState<boolean[][][]>(new Array<boolean[][]>())
     const gridHistoryRef = useRef<boolean[][][]>(gridHistory)
+    // TODO move these starting dimension values to constants or initial calc setup
     const [gridHeightInCells, setGridHeightInCells] = useState<number>(10)
     const [cellHeightInPx, setCellHeightInPx] = useState<number>(38)
     const [isPlaying, setIsPlaying] = useState<boolean>(false)
@@ -35,7 +33,6 @@ const GameOfLife: React.FC<GameOfLifeProps> = ({ width, height }) => {
         let interval: NodeJS.Timeout | undefined
         if (isPlaying) {
             interval = setInterval(() => {
-                console.log("Grid history before ", gridHistory)
                 updateGrid(gridRef.current, gridHistoryRef.current)
             }, 250) // update interval in milliseconds
         } else if (interval !== undefined) {
@@ -58,8 +55,6 @@ const GameOfLife: React.FC<GameOfLifeProps> = ({ width, height }) => {
         // TODO move to constants or initial calc setup
         const GRID_HEIGHT_IN_PIXELS = 380
 
-        // TODO ensure only 1 rerender at end of all changes
-
         // Creates a new blank square grid of [height] x [height]
         const newGrid: boolean[][] = new Array(newGridHeightInCells[0])
             .fill(false)
@@ -67,9 +62,7 @@ const GameOfLife: React.FC<GameOfLifeProps> = ({ width, height }) => {
 
         const newCellHeight = Math.floor(GRID_HEIGHT_IN_PIXELS / newGridHeightInCells[0])
 
-        console.log(newCellHeight)
-
-        // TODO set state vars here - might have to group into 1 object
+        // Clear grid and history, set state height vars to latest values
         setGrid(newGrid)
         setGridHistory([])
         setCellHeightInPx(newCellHeight)
@@ -79,24 +72,22 @@ const GameOfLife: React.FC<GameOfLifeProps> = ({ width, height }) => {
     const updateGrid = (currentGrid: boolean[][], gridHistory: boolean[][][]) => {
         // First add current grid to gridHistory
         const newGridHistory = [...gridHistory, currentGrid]
-
-        console.log("Grid history after ", newGridHistory)
-
-        // Store the updated gridHistory
+        // Then store the updated gridHistory
         setGridHistory(newGridHistory)
         // Then generate next gen and set it as current grid
         setGrid(generateNextGrid(currentGrid))
-     }
+    }
 
     const handlePrevGen = () => {
-        // TODO
         // Do nothing if no prev grids stored in gridHistory array
         if (gridHistory.length === 0) return
+        // Otherwise set current grid to last grid in gridHistory
         setGrid(gridHistory[gridHistory.length - 1])
         setGridHistory(gridHistory.slice(0, gridHistory.length - 1))
     }
 
     const handleResetGrid = () => {
+        // Pause and reset grid and gridHistory
         setIsPlaying(false)
         setGrid((g) => g.map((row) => row.map(() => false)))
         setGridHistory([])
